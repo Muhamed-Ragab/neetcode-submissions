ae25acb6f6de05132525fcf003d4e5e8dd5c7a2d
Mohamed Ragab <mohamedragab1649@gmail.com>
2026-06-01 00:30:31 +0300

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const prefix = [1];
+        const suffix = [1];
+        const res = [];
+
+        for (let i = 0; i < nums.length - 1; ++i) {
+            prefix.push(prefix[i] * nums[i]);
+        }
+
+        for (let i = nums.length - 1; i > 0; --i) {
+            suffix.push(suffix[nums.length - i - 1] * nums[i]);
+        }
+
+        console.log({ prefix, suffix });
+
+        for (let i = 0; i < nums.length; ++i) {
+            res.push(prefix[i] * suffix[nums.length - i - 1]);
+        }
+
+        return res;
+    }
+}
